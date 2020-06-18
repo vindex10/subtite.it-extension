efcb9b127bc3e17562eead6537d153f3b1890749
Victor Ananyev <vindex10@gmail.com>
2020-06-18 04:17:18 +0200

    broken version of secure auth init

--- front-end/content-scripts/content.js
+++ front-end/content-scripts/content.js
@@ -1,140 +1,69 @@
-window.setTimeout(() => {
+let SERVER_NAME = "https://subtite.it"
+//let SERVER_NAME = "http://localhost:5000"
 
+function start(){
+  showEditor();
+}
 
-  browser.runtime.onMessage.addListener(
-    async function(request, sender, sendResponse) {
-      const checkbox = document.querySelector("#switch")
-      if (request.signInData) {
+function showEditor() {
+  document.querySelector('#info-contents').insertAdjacentHTML('beforebegin', `<div class="add-translation">
+          <button class="add-translation__toggle-button">Hide</button>
+          <form class="add-translation__form">
+            <p class="add-translation__text add-translation__prev-text"></p>
+            <textarea class="add-translation__text add-translation__textarea" id="phrase-translation"> </textarea>
+            <p class="add-translation__text add-translation__next-text"></p>
+            <div class="add-translation__submit-button-container">
+              <button class="add-translation__download-button">Download Script</button>
+              <button class="add-translation__submit-button" type="submit">Submit</button>
+            </div>
+        </form>
+      </div>`);
 
-        try {
-          const dataToken = await postData('https://subtite.it/auth', request.signInData);
-          localStorage.setItem('jwt_token', dataToken.access_token);
+  document.querySelector('.add-translation__toggle-button').addEventListener('click', (event)=> {
 
-          try {
-            const phrases = await getData('https://subtite.it/phrase', 'https://www.youtube.com/watch?v=F9ei40nxKDc');
-
-            const phrasesExtended = [...phrases];
-            phrasesExtended.unshift({data: "Script starts here:", ref: 9000});
-            phrasesExtended.push({data: "The end of the script.",  ref: 9000});
-
-            const sortedPhrases= [];
-            for (let i = 0; i < phrases.length; i++) {
-              if (phrases[i].hasOwnProperty('editable')) {
-                sortedPhrases.push({...phrasesExtended[i-1]});
-                sortedPhrases.push({...phrasesExtended[i]});
-                sortedPhrases.push({...phrasesExtended[i+1]})
-              }
-            }
-            document.querySelector('.video-stream').currentTime = sortedPhrases[1].start / 1000;
-
-            start();
-
-            document.querySelector('.add-translation__prev-text').innerText = sortedPhrases[0].data;
-            document.querySelector('.add-translation__textarea').innerText = sortedPhrases[1].data;
-            document.querySelector('.add-translation__textarea').setAttribute('edition_id', `${sortedPhrases[1].edition_id}`);
-            document.querySelector('.add-translation__next-text').innerText = sortedPhrases[2].data;
-
-
-          } catch(error) {
-            console.log('Failed to get phrases:', error);
-          }
-        } catch(error) {
-          console.log('Failed to sign in:', error);
-        }
-
-          document.querySelector('.add-translation__form').addEventListener('submit', async (event) => {
-            event.preventDefault();
-            const data = document.querySelector('#phrase-translation').value;
-            const edition_id = document.querySelector('#phrase-translation').getAttribute('edition_id');
-
-            const translationData = {
-              edition_id,
-              data
-            };
-
-            try {
-              await authorizedPostData('https://subtite.it/phrase', translationData);
-
-              document.querySelector('.add-translation__toggle-button').innerText = 'Show';
-              document.querySelector('.add-translation__form').classList.add('hide-add-translation');
-            } catch(error) {
-              console.log('Translation cannot be saved: ', error)
-            }
-          });
-      }
+    if(!document.querySelector('.add-translation__form').classList.contains('hide-add-translation')) {
+      event.target.innerText = 'Show';
+      document.querySelector('.add-translation__form').classList.add('hide-add-translation');
+    } else {
+      event.target.innerText = 'Hide';
+      document.querySelector('.add-translation__form').classList.remove('hide-add-translation');
     }
-  );
+  })
+}
 
-  function start(){
-    showEditor();
-  }
+function triggerEditor(sortedPhrases) {
+    document.querySelector('.video-stream').currentTime = sortedPhrases[1].start / 1000;
 
-  function showEditor() {
-    document.querySelector('#info-contents').insertAdjacentHTML('beforebegin', `<div class="add-translation">
-            <button class="add-translation__toggle-button">Hide</button>
-            <form class="add-translation__form">
-              <p class="add-translation__text add-translation__prev-text"></p>
-              <textarea class="add-translation__text add-translation__textarea" id="phrase-translation"> </textarea>
-              <p class="add-translation__text add-translation__next-text"></p>
-              <div class="add-translation__submit-button-container">
-                <button class="add-translation__download-button">Download Script</button>
-                <button class="add-translation__submit-button" type="submit">Submit</button>
-              </div>
-          </form>
-        </div>`);
+    start();
 
-      document.querySelector('.add-translation__toggle-button').addEventListener('click', (event)=> {
+    document.querySelector('.add-translation__prev-text').innerText = sortedPhrases[0].data;
+    document.querySelector('.add-translation__textarea').innerText = sortedPhrases[1].data;
+    document.querySelector('.add-translation__textarea').setAttribute('edition_id', `${sortedPhrases[1].edition_id}`);
+    document.querySelector('.add-translation__next-text').innerText = sortedPhrases[2].data;
 
-        if(!document.querySelector('.add-translation__form').classList.contains('hide-add-translation')) {
-          event.target.innerText = 'Show';
-          document.querySelector('.add-translation__form').classList.add('hide-add-translation');
-        } else {
-          event.target.innerText = 'Hide';
-          document.querySelector('.add-translation__form').classList.remove('hide-add-translation');
-        }
-      })
-  }
+    document.querySelector('.add-translation__form').addEventListener('submit', async (event) => {
+      event.preventDefault();
+      const data = document.querySelector('#phrase-translation').value;
+      const edition_id = document.querySelector('#phrase-translation').getAttribute('edition_id');
 
-  async function postData(url, data) {
+      const translationData = {
+        edition_id,
+        data
+      };
 
-    const response = await fetch(url, {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
+      try {
+        browser.runtime.sendMessage({translationData: translationData, action: "submitPhrase"});
+
+        document.querySelector('.add-translation__toggle-button').innerText = 'Show';
+        document.querySelector('.add-translation__form').classList.add('hide-add-translation');
+      } catch(error) {
+        console.log('Translation cannot be saved: ', error)
+      }
     });
-    return await response.json();
-  }
+}
 
-  async function authorizedPostData(url, data) {
-
-    const response = await fetch(url, {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `JWT ${localStorage.getItem('jwt_token')}`
-      },
-      body: JSON.stringify(data)
-    });
-    return await response;
-  }
-
-  async function getData(url, videoLink) {
-    const videoLinkFormatted = videoLink.replace(/&.*/, '');
-    const response = await fetch(url + `?url=${videoLinkFormatted}`, {
-      method: 'GET',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `JWT ${localStorage.getItem('jwt_token')}`,
-      },
-    });
-    return await response.json();
-  }
-
-
-
-}, 2000);
+browser.runtime.onMessage.addListener(request => {
+    if (request.action == "triggerEdit") {
+      triggerEditor(request.sortedPhrases);
+    }
+});
--- front-end/popup/popup.js
+++ front-end/popup/popup.js
@@ -1,51 +1,62 @@
-window.addEventListener('load', ready);
+let SERVER_NAME = "https://subtite.it"
+//let SERVER_NAME = "http://localhost:5000"
+
+let userIdentity = null;
 
 function ready() {
+  init_router();
+  init_signIn();
+}
+
+window.onload = ready;
+
+function init_router() {
+
+  const authPopUp = document.querySelector('#popup-auth');
+  const logInPopUp = document.querySelector('#popup-login');
+  const landIngPopUp = document.querySelector('#popup-landing');
+  const AuthPageLink = document.querySelector('.header__auth-link');
+  const loginPageLink = document.querySelector('.header__login-link');
+  const backHomePageLink = document.querySelector('.header__back-link');
+  const backHome = document.querySelector('.back-link');
 
 
-  // Toggle Landing and LogIn PopUp Blocks;
-  (function() {
+  logInPopUp.style.display = 'none';
+  authPopUp.style.display = 'none';
 
-    const authPopUp = document.querySelector('#popup-auth');
-    const logInPopUp = document.querySelector('#popup-login');
-    const landIngPopUp = document.querySelector('#popup-landing');
-    const AuthPageLink = document.querySelector('.header__auth-link');
-    const loginPageLink = document.querySelector('.header__login-link');
-    const backHomePageLink = document.querySelector('.header__back-link');
-    const backHome = document.querySelector('.back-link');
-
-
+  AuthPageLink.addEventListener('click', () => {
+    landIngPopUp.style.display = 'none';
+    authPopUp.style.display = 'block';
     logInPopUp.style.display = 'none';
+  });
+
+  loginPageLink.addEventListener('click', () => {
+    landIngPopUp.style.display = 'none';
     authPopUp.style.display = 'none';
+    logInPopUp.style.display = 'block';
+  });
 
-    AuthPageLink.addEventListener('click', () => {
-      landIngPopUp.style.display = 'none';
-      authPopUp.style.display = 'block';
-      logInPopUp.style.display = 'none';
-    });
+  const clickHandler = () => {  
+    landIngPopUp.style.display = 'block';
+    authPopUp.style.display = 'none';
+    logInPopUp.style.display = 'none'; 
+  }
 
-    loginPageLink.addEventListener('click', () => {
-      landIngPopUp.style.display = 'none';
-      authPopUp.style.display = 'none';
-      logInPopUp.style.display = 'block';
-    });
+  backHomePageLink.addEventListener('click', () => clickHandler());
+  backHome.addEventListener('click', () => clickHandler());
 
-    const clickHandler = () => {  
-      landIngPopUp.style.display = 'block';
-      authPopUp.style.display = 'none';
-      logInPopUp.style.display = 'none'; 
+}
+
+function init_signIn () {
+  getData(SERVER_NAME + '/profile').then( async (res) => {
+    if (res["username"]) {
+      userIdentity = res;
+      await onLogin();
     }
-
-    backHomePageLink.addEventListener('click', () => clickHandler());
-    backHome.addEventListener('click', () => clickHandler());
-
-  })();
-
-
-  // SignIn Logic:
-  (function () {
+  }).catch(() => {
     let signInData;
-    document.querySelector('#signin-form').addEventListener('submit', () => {
+    // TODO: trigger and intercept submit instead
+    document.querySelector('#signin-form-submit').addEventListener('click', (event) => {
       const username = document.querySelector('#useremail').value;
       const password = document.querySelector('#password').value;
 
@@ -54,12 +65,79 @@ function ready() {
         password
       };
 
-      browser.tabs.query({currentWindow: true, active: true}, function (tabs) {
-        var activeTab = tabs[0];
-        browser.tabs.sendMessage(activeTab.id, {"signInData": signInData});
+      postData(SERVER_NAME + '/auth', signInData).then( async (res) => {
+        if (res["username"]) {
+          userIdentity = res;
+          await onLogin();
+        }
       });
     });
-  })();
-
+  });
 }
 
+async function getAndPreparePhrases(uri) {
+    const phrases = await getVideoData(SERVER_NAME + '/phrase', uri);
+
+    const phrasesExtended = [...phrases];
+    phrasesExtended.unshift({data: "Script starts here:", ref: 9000});
+    phrasesExtended.push({data: "The end of the script.",  ref: 9000});
+
+    const sortedPhrases= [];
+    for (let i = 0; i < phrases.length; i++) {
+      if (phrases[i].hasOwnProperty('editable')) {
+        sortedPhrases.push({...phrasesExtended[i-1]});
+        sortedPhrases.push({...phrasesExtended[i]});
+        sortedPhrases.push({...phrasesExtended[i+1]})
+      }
+    }
+  return sortedPhrases;
+}
+
+async function onLogin() {
+  document.querySelector("#popup-landing .welcome").innerHTML = "Hello, " + (userIdentity.full_name || userIdentity.username) + "!";
+  try {
+    sortedPhrases = await getAndPreparePhrases('https://www.youtube.com/watch?v=F9ei40nxKDc')
+    await browser.tabs.sendMessage(browser.tabs.activeTab.id, {action: "triggerEdit", sortedPhrases: sortedPhrases});
+  } catch(error) {
+    console.log('Failed to get phrases:', error);
+  }
+}
+
+async function submitPhrase(translationData) {
+    await postData(SERVER_NAME + '/phrase', translationData);
+}
+
+browser.runtime.onMessage.addListener(async (data, sender) => {
+  if (data.action == "submitPhrase") {
+    await submitPhrase(data.translationData);
+  }
+});
+
+async function postData(url, data) {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+  return await response.json();
+}
+
+async function getData(url) {
+  const response = await fetch(url, {
+    method: 'GET'
+  });
+  return await response.json();
+}
+
+async function getVideoData(url, videoLink) {
+  const videoLinkFormatted = videoLink.replace(/&.*/, '');
+  const response = await fetch(url + `?url=${videoLinkFormatted}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  });
+  return await response.json();
+}
